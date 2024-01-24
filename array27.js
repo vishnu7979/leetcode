2b6546377046744a257746b00e7807ccc240fe06
vishnu <vishnushaji797@gmail.com>
2024-01-24 12:22:54 +0530

    array27

---
+++ array27.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]!=val){
+            nums[count]=nums[i];
+            count++;
+        }
+    }
+
+    return count
+};
